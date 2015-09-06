183e30d8c1501107a4bf8c38dfc057b731ede954
Andrey Antukh <niwi@niwi.nz>
2015-09-06 13:09:50 +0300

    Remove manifold support and start using cats 1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "BSD (2 Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [funcool/cats "0.6.1"]
-                 [manifold "0.1.0"]]
+                 [funcool/cats "1.0.0-SNAPSHOT"]]
   :deploy-repositories {"releases" :clojars
                         "snapshots" :clojars}
   :source-paths ["src"]
