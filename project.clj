7da256f5dcaffaaeca19c9f61f64c0f8a43b79e5
Andrey Antukh <niwi@niwi.nz>
2015-10-17 17:10:36 +0300

    Set version to 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/promissum "0.3.1"
+(defproject funcool/promissum "0.3.2"
   :description "A composable promise/future library for Clojure."
   :url "https://github.com/funcool/promissum"
   :license {:name "BSD (2 Clause)"
